9efca7a7c8fcdbcfa9c79305811393ad115c6f16
st113264 <sdn2s@outlook.com>
2024-02-18 01:06:56 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -13,117 +13,117 @@ def df(x):
 # процедура отделения корней
 def tabulation_approach(a, b, step):
     intervals = []  # Список для хранения отрезков изменения знака
-    sign_changes = 0 # счетчик изменений знака
+    s_ch = 0 # счетчик изменений знака
 
-    current_x = a
-    while current_x <= b:
-        current_y = f(current_x) # Значение функции в текущей точке
-        next_x = current_x + step # Переход к следующей точке с шагом step
-        next_y = f(next_x) # Значение функции в следующей точке
+    c_x = a
+    while c_x <= b:
+        c_y = f(c_x) # Значение функции в текущей точке
+        n_x = c_x + step # Переход к следующей точке с шагом step
+        n_y = f(n_x) # Значение функции в следующей точке
 
-        if current_y * next_y <= 0:  # Проверка на изменение знака
-            intervals.append((current_x, next_x))
-            sign_changes += 1
+        if c_y * n_y <= 0:  # Проверка на изменение знака
+            intervals.append((c_x, n_x))
+            s_ch += 1
 
-        current_x = next_x # Переопределение текущей точки для следующей итерации
+        c_x = n_x # Переопределение текущей точки для следующей итерации
 
     print(f"Отрезки изменения знака: {', '.join(map(lambda interval: f'[{interval[0]:.4f}, {interval[1]:.4f}]', intervals))}")
-    print(f"Количество отрезков изменения знака: {sign_changes}")
+    print(f"Количество отрезков изменения знака: {s_ch}")
 # Метод бисекции
-def bisection (a,b,n, eps): # отрезок от a до b делим на n частей, погрешность eps
-    assert a!=0,  'a равно 0'
-    assert b!=0, 'b равно 0'
-    count = 0 # счетчик количества итераций для достижения точности epsilon
-    # сначала отделим корни
-    setka=np.linspace(a, b, n)
-    # далее уточним корни
-    for x,y in zip(setka, setka[1:]):
-        if f(x) * f(y) > 0: # если на отрезке нет корня, смотрим следующий
+def bisection(a, b, n, eps):
+    assert a != 0, 'a равно 0'
+    assert b != 0, 'b равно 0'
+    count = 0  # счетчик количества итераций для достижения точности epsilon
+    # отделение корней
+    s = np.linspace(a, b, n)
+    roots = []
+    # уточнение корней
+    for x, y in zip(s, s[1:]):
+        if f(x) * f(y) > 0:  # если на отрезке нет корня, смотрим следующий
             continue
         root = None
-        while ( abs(f(y)-f(x)) )>eps:     # пока отрезок больше заданной погрешности, выполняем нижестоящие операции:
-            mid = (y+x)/2                   # получаем середину отрезка
-            if f(mid) == 0 or f(mid)<eps:    # если функция в середине отрезка равну нулю или меньше погрешности:
-                root = mid                  # корень равень серединному значению
+        while abs(f(y) - f(x)) > eps:  # пока отрезок больше заданной погрешности:
+            mid = (y + x) / 2  # получаем середину отрезка
+            if f(mid) == 0 or f(mid) < eps:  # если функция в середине отрезка равна нулю или меньше погрешности:
+                root = mid  # корень равен среднему значению
                 break
-            elif (f(mid) * f(x)) < 0:       # иначе если произведение функции в середине отрезка на функцию в т. а <0
-                y = mid                     # серединой становится точка b
+            elif f(mid) * f(x) < 0:  # выбор новой середины
+                y = mid  # серединой становится точка b
             else:
-                x = mid                     #в другом случае - точка а
+                x = mid  # иначе точка a
             count += 1
         if root:
-            yield root
+            roots.append(root)
     print("Количество шагов для достижения точности epsilon:", count)
+    return roots
 # Реализация метода Ньютона для нахождения корня функции
-def newton_method(initial_guess, eps):
+def newton(initial_guess, eps):
     x0 = initial_guess # начальное предположение
     count = 0 # счетчик итераций
 
     while True:
-        x1 = x0 - f(x0) / df(x0) # основная формула метода Ньютона
+        x = x0 - f(x0) / df(x0) # основная формула метода Ньютона
         count += 1
 
-        if abs(x1 - x0) < eps: # проверка точности
+        if abs(x - x0) < eps: # проверка точности
             break
 
-        x0 = x1 # изменение предположения
+        x0 = x # изменение предположения
 
-    return x1, count
+    return x, count
 # Модифицированный метод Ньютона
-def modified_newton(f, df, x0, eps = 1e-5, max_iter=1000):
+def modified_newton(f, df, x0, eps = 1e-5):
     x = x0
     iteration = 0
 
     while True:
-        x_new = x - f(x) / df(x0) # обновление значения x  по формуле Ньютона
+        x_n = x - f(x) / df(x0) # обновление значения x  по формуле Ньютона
         iteration += 1
-        # проверка условия сходимости или достижения максимального количества итераций
-        if abs(x_new - x) < eps or iteration >= max_iter:
+        # проверка условия сходимости
+        if abs(x_n - x) < eps:
             break
 
-        x = x_new
+        x = x_n
 
-    return x_new, iteration
-def secant_method(f, x0, x1, eps = 1e-5, max_iter=100):
+    return x_n, iteration
+def secant_method(f, a, b, eps = 1e-5):
     iterations = 0
+    # Вычисление нового приближения x2 с использованием метода секущих
+    while True:
+        x = b - f(b) * (b - a) / (f(b) - f(a))
+        # Проверка на достижение требуемой точности
+        if abs(x - b) < eps:
+            return x, iterations # Если точность достигнута, возвращаем найденное значение и количество итераций
 
-    while iterations < max_iter:
-        x2 = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0))
-
-        if abs(x2 - x1) < eps:
-            return x2, iterations
-
-        x0 = x1
-        x1 = x2
+        a = b
+        b = x
         iterations += 1
 
-    raise Exception("Не удалось найти корень методом секущих")
 print("Функция f(x): 10 * cos(x) - 0.1 * x**2")
 print("Параметры:\nЛевая граница: {}\nПравая граница: {}\nТочность: {}".format(left_border, right_border, eps))
+
 print("Метод бисекции:")
 tabulation_approach(-8, 2, 0.1)
 res = list(bisection(-8,2,1000, 0.00001))
 print('Корни по методу бисекции находятся в точках:')
 print(', '.join(map(lambda x: f'{x:.4f}', res)))
+
 print("Метод Ньютона:")
 initial_guess = (left_border+right_border)/2  # начальное предположение
-
-root, iterations = newton_method(initial_guess, eps)
-
+root, iterations = newton(initial_guess, eps)
 print(f"Корень методом Ньютона: {root:.6f}")
 print(f"Количество итераций: {iterations}")
 
 print("Модифицированный метод Ньютона:")
 x0 = 1.5
 solution, iterations = modified_newton(f, df, x0)
-
 print("Корень: {:.7f}".format(solution))
 print("Количество итераций:", iterations)
-print("Метод секущих: ")
-x0 = 1
-x1 = 2
 
-solution_secant, iterations_secant = secant_method(f, x0, x1)
+print("Метод секущих: ")
+a = 1
+b = 2
+solution_secant, iterations_secant = secant_method(f, a, b)
 print("Корень уравнения {:.7f}:".format(solution_secant))
 print("Количество итераций:", iterations_secant)
 
